740c4368364961757a39f3fb325cd9cb43a42387
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-09-04 17:55:09 -0500

    served files

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+function greet(req, res){
+    res.sendFile(absolutePath)
+}
+
+const absolutePath = __dirname + "/views/index.html"
+
+app.get("/", greet)
+
 
 
 
